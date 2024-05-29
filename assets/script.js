e6df719321c2a25eac29c3bc8ef9d3e41eb2237a
asw <annasilverwolff@gmail.com>
2024-05-28 21:38:03 -0400

    new search bug introduced

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 let map;
 
-// 28.601850034893445, -81.19759825701948
+const APIKEY = "AIzaSyDiGQQfGOQhaN__gJWQzYbepqVG05NmqTI"
+
+const url = "https://cors-anywhere-jung-48d4feb9d097.herokuapp.com/" + "https://maps.googleapis.com/maps/api/js?key=AIzaSyDiGQQfGOQhaN__gJWQzYbepqVG05NmqTI&loading=async&libraries=places&callback=initMap";
+
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
@@ -11,10 +14,13 @@ async function initMap() {
   });
 }
 
-
-
-
 initMap();
+
+// fetch(url).then(response => {
+//   return response.json()
+// }).then(data => {
+//   console.log(data);
+// });
 
 // function searchBar
 // currentData = {
@@ -24,17 +30,42 @@ initMap();
 // }
 
 
-const input = document.getElementById("searchBar");
-const searchBox = new google.maps.places.SearchBox(input);
+// const input = document.getElementById("searchBar");
+// const searchBox = new google.maps.places.SearchBox(input);
 
-findPlaceFromQuery(request, function(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for ( var i= 0; i <results.length; i++) {
-      createMarker(results[i]);
-    }
-    map.setCenter(results[0].geometry.location);
-  }
-})
+// findPlaceFromQuery(request, function(results, status) {
+//   if (status === google.maps.places.PlacesServiceStatus.OK) {
+//     for ( var i= 0; i <results.length; i++) {
+//       createMarker(results[i]);
+//     }
+//     map.setCenter(results[0].geometry.location);
+//   }
+// })
+
+
+
+// map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+// // Bias the SearchBox results towards current map's viewport.
+// map.addListener("bounds_changed", () => {
+//   searchBox.setBounds(map.getBounds());
+// });
+
+// let markers = [];
+
+// // Listen for the event fired when the user selects a prediction and retrieve
+// // more details for that place.
+// searchBox.addListener("places_changed", () => {
+//   const places = searchBox.getPlaces();
+
+//   if (places.length == 0) {
+//     return;
+//   }
+
+//   // Clear out the old markers.
+//   markers.forEach((marker) => {
+//     marker.setMap(null);
+//   });
+//   markers = [];
 
 //nearby search data?
-console.log(currentData);
+//console.Log(currentData);
