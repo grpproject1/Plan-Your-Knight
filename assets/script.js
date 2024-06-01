b506f2e56889c0ab53d5148494b4b1fbf3791645
Erin DeVine <erinj291@gmail.com>
2024-06-01 16:51:12 -0400

    hardcoding information for coordinates parking+food and moved some commented code to the test file

--- assets/script.js
+++ assets/script.js
@@ -64,27 +64,172 @@ async function initMap() {
     content:'<h1>bork bork</h1>'
   });
   
-  const p1 = addMarker({lat: 28.59931385, lng: -81.19764343879355,});
-  const p2 = addMarker({lat: 28.5989923, lng: -81.19753491921549,});
-  const p3 = addMarker({lat: 28.59965215, lng: -81.1977661889483,});
-  const p4 = addMarker({lat: 28.59753485, lng: -81.19666027674731,});
-  const p5 = addMarker({lat: 28.59726535, lng: -81.1974185836448,});
-  const p6 = addMarker({lat: 28.5972515, lng: -81.19732777976934,});
-  const p7 = addMarker({lat: 28.59729765, lng: -81.19658849689151,});
-  const p8 = addMarker({lat: 28.596887000000002, lng: -81.196147443348,});
-  const p9 = addMarker({lat: 28.601376700000003, lng: -81.19602257741718,});
-  const p10 = addMarker({lat: 28.59702355, lng: -81.1989022761598,});
-  const p11 = addMarker({lat: 28.596522, lng: -81.19800161744283,});
-  const p12 = addMarker({lat: 28.5964478, lng: -81.19591531228271,});
-  const p13 = addMarker({lat: 28.5961117, lng: -81.19668386523279,});
-  const p14 = addMarker({lat: 28.5964679, lng: -81.1967058,});
-  const p15 = addMarker({lat: 28.5963482, lng: -81.1969695,});
-  const p16 = addMarker({lat: 28.60256305, lng: -81.19571185092033,});
-  const p17 = addMarker({lat: 28.6017295, lng: -81.1963112,});
-  const p18 = addMarker({lat: 28.6017437, lng: -81.1963215,});
-  const p19 = addMarker({lat: 28.59690265, lng: -81.20040525132633,});
-  const p20 = addMarker({lat: 28.601756, lng: -81.1958105,});
-  
+// Hardcoded parking (nearest 20 within 1 mile)
+const p1 = addMarker({
+  coords:{lat: 28.59931385, lng: -81.19764343879355,},
+  content:'<h1>Parking</h1>'
+});
+const p2 = addMarker({
+  coords:{lat: 28.5989923, lng: -81.19753491921549,},
+  content:'<h1>Parking</h1>'
+});
+const p3 = addMarker({
+  coords:{lat: 28.59965215, lng: -81.1977661889483,},
+  content:'<h1>Parking</h1>'
+});
+const p4 = addMarker({
+  coords:{lat: 28.59753485, lng: -81.19666027674731,},
+  content:'<h1>Parking</h1>'
+});
+const p5 = addMarker({
+  coords:{lat: 28.59726535, lng: -81.1974185836448,},
+  content:'<h1>Parking</h1>'
+});
+const p6 = addMarker({
+  coords:{lat: 28.5972515, lng: -81.19732777976934,},
+  content:'<h1>Parking</h1>'
+});
+const p7 = addMarker({
+  coords:{lat: 28.59729765, lng: -81.19658849689151,},
+  content:'<h1>Parking</h1>'
+});
+const p8 = addMarker({
+  coords:{lat: 28.596887000000002, lng: -81.196147443348,},
+  content:'<h1>Parking</h1>'
+});
+const p9 = addMarker({
+  coords:{lat: 28.601376700000003, lng: -81.19602257741718,},
+  content:'<h1>Parking</h1>'
+});
+const p10 = addMarker({
+  coords:{lat: 28.59702355, lng: -81.1989022761598,},
+  content:'<h1>Parking</h1>'
+});
+const p11 = addMarker({
+  coords:{lat: 28.596522, lng: -81.19800161744283,},
+  content:'<h1>Parking</h1>'
+});
+const p12 = addMarker({
+  coords:{lat: 28.5964478, lng: -81.19591531228271,},
+  content:'<h1>Parking</h1>'
+});
+const p13 = addMarker({
+  coords:{lat: 28.5961117, lng: -81.19668386523279,},
+  content:'<h1>Parking</h1>'
+});
+const p14 = addMarker({
+  coords:{lat: 28.5964679, lng: -81.1967058,},
+  content:'<h1>Parking</h1>'
+});
+const p15 = addMarker({
+  coords:{lat: 28.5963482, lng: -81.1969695,},
+  content:'<h1>Parking</h1>'
+});
+const p16 = addMarker({
+  coords:{lat: 28.60256305, lng: -81.19571185092033,},
+  content:'<h1>Parking</h1>'
+});
+const p17 = addMarker({
+  coords:{lat: 28.6017295, lng: -81.1963112,},
+  content:'<h1>Parking</h1>'
+});
+const p18 = addMarker({
+  coords:{lat: 28.6017437, lng: -81.1963215,},
+  content:'<h1>Parking</h1>'
+});
+const p19 = addMarker({
+  coords:{lat: 28.59690265, lng: -81.20040525132633,},
+  content:'<h1>Parking</h1>'
+});
+const p20 = addMarker({
+  coords:{lat: 28.601756, lng: -81.1958105,},
+  content:'<h1>Parking</h1>'
+});
+
+
+
+
+// Hardcoded restuarants (nearest 20 within 1 mile)
+const r1 = addMarker({
+  coords:{lat: 28.5979074, lng: -81.1998033,},
+  content:'<h1>63 South</h1>'
+});
+const r2 = addMarker({
+  coords:{lat: 28.5960256, lng: -81.19875738712756,},
+  content:'<h1>Chick-fil-A</h1>'
+});
+const r3 = addMarker({
+  coords:{lat: 28.5958111, lng: -81.1991321,},
+  content:'<h1>Smoothie King</h1>'
+});
+const r4 = addMarker({
+  coords:{lat: 28.599339049999998, lng: -81.20132644502809,},
+  content:'<h1>Toppers</h1>'
+});
+const r5 = addMarker({
+  coords:{lat: 28.6016101, lng: -81.2004312,},
+  content:'<h1>Smoothie King</h1>'
+});
+const r6 = addMarker({
+  coords:{lat: 28.6017791, lng: -81.2003765,},
+  content:'<h1>Smoke & Donuts BBQ</h1>'
+});
+const r7 = addMarker({
+  coords:{lat: 28.6021284, lng: -81.2003113,},
+  content:'<h1>Which Wich?</h1>'
+});
+const r8 = addMarker({
+  coords:{lat: 28.6010937, lng: -81.2012939,},
+  content:'<h1>Dominos</h1>'
+});
+const r9 = addMarker({
+  coords:{lat: 28.6020943, lng: -81.2004807,},
+  content:'<h1>Bento Sushi</h1>'
+});
+const r10 = addMarker({
+  coords:{lat: 28.6021939, lng: -81.2003916,},
+  content:'<h1>Huey Magoos</h1>'
+});
+const r11 = addMarker({
+  coords:{lat: 28.601277, lng: -81.2012624,},
+  content:'<h1>Chick-fil-A</h1>'
+});
+const r12 = addMarker({
+  coords:{lat: 28.6022033, lng: -81.2005073,},
+  content:'<h1>Panda Express</h1>'
+});
+const r13 = addMarker({
+  coords:{lat: 28.6021041, lng: -81.2006699,},
+  content:'<h1>Steak n Shake</h1>'
+});
+const r14 = addMarker({
+  coords:{lat: 28.6021694, lng: -81.2006064,},
+  content:'<h1>Qdoba</h1>'
+});
+const r15 = addMarker({
+  coords:{lat: 28.6067927, lng: -81.1970696,},
+  content:'<h1>Jimmy Johns</h1>'
+});
+const r16 = addMarker({
+  coords:{lat: 28.6067323, lng: -81.1985877,},
+  content:'<h1>Dunkin</h1>'
+});
+const r17 = addMarker({
+  coords:{lat: 28.6068082, lng: -81.1985488,},
+  content:'<h1>Subway</h1>'
+});
+const r18 = addMarker({
+  coords:{lat: 28.6070409, lng: -81.1965718,},
+  content:'<h1>Burger U</h1>'
+});
+const r19 = addMarker({
+  coords:{lat: 28.6070156, lng: -81.1984234,},
+  content:'<h1>Dominoak</h1>'
+});
+const r20 = addMarker({
+  coords:{lat: 28.607229, lng: -81.1982791,},
+  content:'<h1>Gringos Locos</h1>'
+});   
 
     //Add Marker Function
     //in order to add a custom image 
@@ -123,111 +268,6 @@ async function initMap() {
 }
 
 initMap();
-
-
-
-
-
-//Name
-//Latitude, Longitude
-//Image URL
-// ScaledSize width, height
-
-// const markers = [
-//   [
-//     "Knightstop",
-//     28.601882536369722,
-//     -81.20066893398244,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Steak 'n Shake",
-//     28.601904608458174,
-//     -81.20074108245133,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Smoothie King",
-//     28.601727991474807,
-//     -81.2004205584811,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Purple Ocean",
-//     28.60181806617204,
-//     -81.2009459361654,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Which Wich",
-//     28.602295519660316,
-//     -81.20028704211859,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Huey Magoo's Chicken Tenders",
-//     28.60193226247241,
-//     -81.20012126154299,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Starbucks",
-//     28.602023173142335,
-//     -81.20025877273342,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ],
-//   [
-//     "Panda Express",
-//     28.601914047996697,
-//     -81.20072470188155,
-//     "marker-icon.png",
-//     38,
-//     31
-//   ]
-// ];
-
-// for (let i = 0; i<markers.length; i++){
-//   const currMarker = markers[i];
-//   const marker = new google.maps.Marker({
-//     position: {lat: currMarker[1], lng: currMarker[2]},
-//     map,
-//     title:currMarker[0],
-//     icon: {
-//       url:currMarker[3],
-//       scaledSize: new google.maps.Size(currMarker[4], currMarker[5])
-//     },
-//     animation: google.maps.Animation.DROP
-// });
-// }
-
-
-
-
-// const restaurants = [
-//   ["Knightstop", 28.601882536369722, -81.20066893398244],
-//   ["Steak 'n Shake", 28.601904608458174, -81.20074108245133],
-//   ["Smoothie King", 28.601727991474807, -81.2004205584811],
-//   ["Purple Ocean", 28.60181806617204, -81.2009459361654],
-//   ["Which Wich",28.602044135665917, -81.20035316796755],
-//   ["Huey Magoo's Chicken Tenders", 28.60193226247241, -81.20012126154299],
-//   ["Starbucks",28.602023173142335, -81.20025877273342],
-//   ["Panda Express",28.601914047996697, -81.20072470188155],
-// ];
-
 
 
 
@@ -321,7 +361,7 @@ fetch(parkingURL)
 
 
   // for restaurant locations:
-  const restaurantsURL = "https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=circle:-81.197125012,28.59899755,1610&bias=proximity:-81.197125012,28.59899755&limit=20&apiKey=d3720612c9c24768b1672bff3ae0a512";
+  const restaurantsURL = "https://api.geoapify.com/v2/places?categories=catering.restaurant,catering.fast_food&filter=circle:-81.197125012,28.59899755,1610&bias=proximity:-81.197125012,28.59899755&limit=20&apiKey=d3720612c9c24768b1672bff3ae0a512";
 
   fetch(restaurantsURL)
     .then(function(response) {
