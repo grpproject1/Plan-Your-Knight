8b9a7c84f9d018a7ce12cfea556f92b1a6ec89ef
asw <annasilverwolff@gmail.com>
2024-06-02 16:27:48 -0400

    added back radio(sorry erica)

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,17 +1,7 @@
 //file for form function if needed. 
-const clickyButton = document.querySelector('#clicky');
 
 // clickyButton.addEventListener('click',
 //     console.log('click');
 // window.location.href = "./"
 // );
 
-
-const container = document.querySelector('.container');
-
-const signUpBtn = document.querySelector('.green-bg button');
-
-
-signUpBtn.addEventListener('click', () => {
-    container.classList.toggle('change')
-});
